02047fb2cde94aca655f6028693066275e53cee9
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-18 18:46:33 +0100

    Working implementation

--- src/main/java/org/wenny/kenny/Main.java
+++ src/main/java/org/wenny/kenny/Main.java
@@ -2,10 +2,17 @@ package org.wenny.kenny;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPrimeNumber(2));
+        System.out.println(isPrimeNumber(2)); // True
+        System.out.println(isPrimeNumber(4)); // False
+        System.out.println(isPrimeNumber(37)); // True
     }
 
     public static boolean isPrimeNumber(int number) {
+        for (var i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
